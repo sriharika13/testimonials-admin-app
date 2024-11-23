944fd4735b7212d905d455adb47e0c670f6a82ab
sriharika.mariyala <sriharika.mariyala@infor.com>
2024-11-23 17:24:33 +0530

    fixed some errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,10 @@
+'use client'
 import { Inter } from "next/font/google";
 import { usePathname } from "next/navigation";
 import "./globals.css";
 import Navbar from '../components/Navbar';
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = {
-  title: "Testimonials",
-  description: "Get testimonials from your customers",
-};
 
 export default function RootLayout({ children }) {
   const path = usePathname();
